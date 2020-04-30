20fab649ffd5db2d615f38ed429ef3246a3bfb65
Hassane Sow <imthassane@Gmail.com>
2020-04-30 19:33:01 +0200

    Adding the tests for the frequency service

---
+++ api/promises/test/router/frequencies_test.exs
@@ -0,0 +1,39 @@
+defmodule Router.FrequenciesTest do
+  use ExUnit.Case
+  use Plug.Test
+
+  alias Router
+
+  @options Router.init([])
+
+  test "Trying to add a frequency with non existant promise" do
+    conn =
+      :post
+      |> conn("/frequency/create/abde", "")
+      |> Router.call(@options)
+
+    assert conn.state == :sent
+    assert conn.status == 404
+  end
+
+  test "Trying to update a frequency with non valid id" do
+    conn =
+      :put
+      |> conn("/frequency/update/abde", "")
+      |> Router.call(@options)
+
+    assert conn.state == :sent
+    assert conn.status == 400
+  end
+
+  test "Trying to delete a non existant frequency" do
+    conn =
+      :delete
+      |> conn("/frequency/delete/abde", "")
+      |> Router.call(@options)
+
+    assert conn.state == :sent
+    assert conn.status == 404
+  end
+
+end
