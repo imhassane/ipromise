11996c3a04f64015cb6da04fc61ed75d768b4b36
Hassane Sow <imthassane@Gmail.com>
2020-04-30 16:56:07 +0200

    Adding the repository for the frequency

--- api/promises/lib/repo/frequency_repo.ex
+++ api/promises/lib/repo/frequency_repo.ex
@@ -1,8 +1,24 @@
 defmodule Repo.FrequencyRepo do
 
-  # TODO: Adding a new Frequency.
+  @collection "frequencies"
 
-  # TODO: Updating the frequency.
+  # Get a promise's frequency.
+  def get_frequency(promise_id) do
+    :database |> Mongo.find_one(@collection, %{ "promise" => id })
+  end
 
-  # TODO: deleting the frequency.
+  # Adding a new Frequency.
+  def add_frequency(frequency) do
+    :database |> Mongo.insert_one!(@collection, frequency)
+  end
+
+  # Updating the frequency.
+  def update_frequency(%{ "_id" => id } = frequency) do
+    :database |> Mongo.update_one(@collection, %{ "_id" => id }, %{ "$set": frequency })
+  end
+
+  # deleting the frequency.
+  def delete_frequency(id) do
+    :database |> Mongo.delete_one(@collection, %{ "_id" => id })
+  end
 end
--- api/promises/lib/repo/target_repo.ex
+++ api/promises/lib/repo/target_repo.ex
@@ -1,5 +1,9 @@
 defmodule Repo.TargetRepo do
 
+  @collection "targets"
+
+   # TODO: Getting targets for a frequency.
+
    # TODO: Adding a new target.
 
    # TODO: Updating the target.
