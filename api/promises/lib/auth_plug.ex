984242484b7bd53d8f371b1205ea1fa3931f1fc9
Hassane Sow <imthassane@Gmail.com>
2020-05-15 22:14:03 +0200

    Adding the authentication middleware

--- api/promises/lib/auth_plug.ex
+++ api/promises/lib/auth_plug.ex
@@ -1,11 +1,11 @@
 defmodule Promises.Auth.AuthPlug do
   defmodule NonAuthenticatedError do
-    @moduledoc """
-    Error raised when the user is not authenticated
-    """
 
     defexception message: "You need to be authenticated", code: 401
   end
+
+  @url "http://localhost:5000/api/v1/user"
+  @headers []
 
   def init(options), do: options
 
@@ -14,18 +14,36 @@ defmodule Promises.Auth.AuthPlug do
     conn
   end
 
-  defp verify_authentication(conn) do
-    is_authenticated =
-      case HTTPoison.get "http://localhost:5000/api/v1/user/" do
-        {:error, _} ->
-          false
-        {:ok, %HTTPoison.Response{body: body}} ->
-          IO.inspect body
-          true
-          _ ->
-          false
-      end
+  defp verify_authentication(%Plug.Conn{req_headers: headers} = conn) do
+    is_authenticated = false
 
-    unless is_authenticated, do: raise(NonAuthenticatedError)
+    {_, token} = get_authentication_token headers
+
+    if !token do
+      raise(NonAuthenticatedError)
+    else
+      headers = [
+        {"content-type", "application/json"},
+        {"authentication-token", token}
+      ]
+      case HTTPoison.get(@url, headers) do
+        {:error, _} ->
+          raise(NonAuthenticatedError)
+        {:ok, %HTTPoison.Response{body: body}} ->
+          conn
+        _ ->
+          raise(NonAuthenticatedError)
+      end
+    end
+
+  end
+
+  defp get_authentication_token(headers) do
+    result = Enum.filter(headers, fn({name, _}) -> name == "authentication-token" end)
+    if (Kernel.length result) == 0 do
+      {"authentication-token", nil}
+    else
+      Kernel.hd result
+    end
   end
 end
